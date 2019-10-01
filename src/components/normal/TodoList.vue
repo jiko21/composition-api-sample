4ff914b2e191829508c0816da8defc721e0e1f73
daikikojima <daikikojima0531@icloud.com>
2019-10-01 21:56:11 +0900

    A: Todo form for normal

---
+++ src/components/normal/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="todo-list">
+    <ul>
+      <li v-for="(todo, i) in todos" :key="i">
+        {{ todo }}
+        <button @click="onDelete(i)">Delete</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { createComponent, PropType } from '@vue/composition-api';
+
+const TodoList = createComponent({
+  props: {
+    todos: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+    delete: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup(props) {
+    const onDelete = (index: number) => {
+      props.delete(index);
+    };
+    return {
+      onDelete,
+    };
+  }
+});
+
+export default TodoList;
+</script>
+
+<style scoped>
+/* NO NEED TO ADAPT DESIGN */
+</style>
