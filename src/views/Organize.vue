47082047552953aca9cd0902a85226af77351572
daikikojima <daikikojima0531@icloud.com>
2019-10-02 18:39:18 +0900

    A: View for organize

---
+++ src/views/Organize.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Todo Apps with Code Organization</h1>
+    <Todo />
+    <SubForm/>
+  </div>
+</template>
+<script lang="ts">
+import {createComponent} from "@vue/composition-api";
+import Todo from '@/components/organize/Todo.vue';
+import SubForm from '@/components/organize/SubForm.vue';
+
+export default createComponent({
+  components: {
+    Todo,
+    SubForm,
+  },
+});
+</script>
