6935fb4a4c83054248fc6862392e3c30a68f4768
daikikojima <daikikojima0531@icloud.com>
2019-10-02 18:37:56 +0900

    A: SubForm form for organize

---
+++ src/components/organize/SubForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="Todo">
+    <h2>Sub form</h2>
+    <input v-model="state.todo" />
+    <button @click="onSubmit">追加</button>
+    <ul>
+      <li v-for="(todo, i) in todos" :key="i">
+        {{todo}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import {createComponent, reactive} from '@vue/composition-api';
+import useTodos from './Store';
+
+const Todo = createComponent({
+  setup() {
+    const {addTodo, todos} = useTodos();
+    const state = reactive({
+      todo: '',
+    });
+    const onSubmit = () => {
+      addTodo(state.todo);
+      state.todo = '';
+    };
+    return {
+      state,
+      onSubmit,
+      todos,
+    };
+  },
+});
+
+export default Todo;
+</script>
+
+<style scoped>
+
+</style>
