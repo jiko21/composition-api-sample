3c7202df9a049edfa28524b90af0f3ffb28bfbb9
daikikojima <daikikojima0531@icloud.com>
2019-10-02 19:22:41 +0900

    A: router

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/organize">Code organization</router-link> |
+      <router-link to="/same-store">Code organization (same store)</router-link>
     </div>
     <router-view />
   </div>
