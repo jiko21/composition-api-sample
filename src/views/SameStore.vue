4af6c64b3c1950d1d4be3ee64710666b9caa70a2
daikikojima <daikikojima0531@icloud.com>
2019-10-02 19:21:46 +0900

    A: store for same-store

---
+++ src/views/SameStore.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Todo Apps with Code Organization</h1>
+    <Todo />
+    <SubForm/>
+  </div>
+</template>
+<script lang="ts">
+import {createComponent} from "@vue/composition-api";
+import Todo from '@/components/same_store/Todo.vue';
+import SubForm from '@/components/same_store/SubForm.vue';
+
+export default createComponent({
+  components: {
+    Todo,
+    SubForm,
+  },
+});
+</script>
