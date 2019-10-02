6129ed658e1c5607916319cb66c79cf99c7ffda4
daikikojima <daikikojima0531@icloud.com>
2019-10-02 18:37:15 +0900

    A: TodoForm form for normal

---
+++ src/components/organize/TodoForm.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="todo-form">
+    <input v-model="state.text"/>
+    <button @click="submit">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import {createComponent, PropType, reactive} from '@vue/composition-api';
+
+const TodoForm = createComponent({
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true,
+    },
+  },
+  setup(props) {
+    const state = reactive({
+      text: '',
+    });
+
+    const submit = () => {
+      props.onSubmit(state.text);
+      state.text = '';
+    };
+
+    return {
+      state,
+      submit,
+    };
+  }
+});
+
+export default TodoForm;
+</script>
+
+<style scoped>
+
+</style>
